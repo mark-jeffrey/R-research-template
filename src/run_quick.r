e764fecddfb9dfc7c74e5f842fb4b309f9adbce2
Mark <84331018+Hobnobs9444@users.noreply.github.com>
2023-07-02 14:56:25 +0100

    Update run_quick.r
    Source encoding explicit

--- src/run_quick.r
+++ src/run_quick.r
@@ -5,7 +5,6 @@ library(here)
 scripts <- list.files(here("scripts"), pattern = "*.r$", full.names = TRUE)
 
 for (script in scripts) {
-    print(paste("Sourcing", script, "..."), encoding = "UTF-8")
-    source(script, echo = FALSE, local = TRUE)
-    rm(list = ls())
+    print(paste("Sourcing", script, "..."))
+    source(script, echo = FALSE, local = TRUE, encoding = "UTF-8"))
 }
